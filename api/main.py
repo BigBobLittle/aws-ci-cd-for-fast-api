dbd05479e126659d1a36a3e15e1330294c62e4a0
BigBobLittle <littletheprogrammer@gmail.com>
2024-04-13 00:16:37 +0100

    feat: add extra endpoint

--- api/main.py
+++ api/main.py
@@ -7,4 +7,8 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
 handler = Mangum(app=app)
